e4fa5d86fa5f30c64d72a805c6dd389b7dbd68a5
CB2222124 <2222124@student.uwtsd.ac.uk>
2023-04-19 12:16:32 +0100

    Dockerise frontend and add database

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,4 +6,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true
+    },
+    host: true,
+    strictPort: true,
+    port: 9000
+  }
 })
