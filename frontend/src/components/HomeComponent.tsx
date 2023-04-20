527fa0fd8abc4aebb249fab2cc05bd0dc95288bd
CB222124 <2222124@student.uwtsd.ac.uk>
2023-04-21 00:14:32 +0100

    Home page progress

--- frontend/src/components/HomeComponent.tsx
+++ frontend/src/components/HomeComponent.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import '../styles/home.css'
 import '../styles/registration.css'
 
 function HomeComponent() {
     return (
-        <div className="d-flex flex-column align-items-center justify-content-center" style={{height: '50vh'}}>
+        <form className="d-flex flex-column align-items-center justify-content-center" style={{height: '50vh'}}>
             <h2>Find your Personalised Registration</h2>
-            <form>
-                <input className="form-control registration" placeholder="Search" aria-label="Search"/>
-            </form>
-        </div>
+            <input className="form-control registration search" placeholder="Search" aria-label="Search"/>
+            <button className='btn btn-success bg-gradient search search-button mt-2'><b>Search</b></button>
+            <span className='text-success mt-2'>Advanced Search</span>
+        </form>
     )
 }
 
