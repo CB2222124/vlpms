63176b870b532dd1c6ad6c76f07d7f42cdc438f1
CB222124 <2222124@student.uwtsd.ac.uk>
2023-04-29 23:34:09 +0100

    Add customer ID as pkey

--- backend/src/main/java/com/github/cb2222124/vlpms/backend/model/Customer.java
+++ backend/src/main/java/com/github/cb2222124/vlpms/backend/model/Customer.java
@@ -1,10 +1,8 @@
 package com.github.cb2222124.vlpms.backend.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,12 +15,14 @@ import java.util.List;
 @Getter
 @Setter
 public class Customer {
+
     @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(unique = true)
     private String username;
-
-    //private String password;
-
-    //private String paymentGatewayToken;
 
     @OneToMany(fetch = FetchType.LAZY)
     private List<Registration> ownedRegistrations;
--- database/create-tables.sql
+++ database/create-tables.sql
@@ -1,5 +1,6 @@
 CREATE TABLE customer
 (
+    id       SERIAL PRIMARY KEY,
     username character varying(255) NOT NULL
 );
 
@@ -17,18 +18,15 @@ CREATE TABLE listing
 
 CREATE TABLE customer_owned_registrations
 (
-    customer_username                character varying(255) NOT NULL,
+    customer_id                      integer,
     owned_registrations_registration character varying(255) NOT NULL
 );
 
 CREATE TABLE customer_wishlisted_listings
 (
-    customer_username                             character varying(255) NOT NULL,
+    customer_id                                   integer,
     wishlisted_listings_registration_registration character varying(255) NOT NULL
 );
-
-ALTER TABLE ONLY customer
-    ADD CONSTRAINT customer_pkey PRIMARY KEY (username);
 
 ALTER TABLE ONLY listing
     ADD CONSTRAINT listing_pkey PRIMARY KEY (registration_registration);
@@ -37,13 +35,13 @@ ALTER TABLE ONLY registration
     ADD CONSTRAINT registration_pkey PRIMARY KEY (registration);
 
 ALTER TABLE ONLY customer_owned_registrations
-    ADD CONSTRAINT customer_owned_registrations_customer_fkey FOREIGN KEY (customer_username) REFERENCES customer (username);
+    ADD CONSTRAINT customer_owned_registrations_customer_fkey FOREIGN KEY (customer_id) REFERENCES customer (id);
 
 ALTER TABLE ONLY customer_owned_registrations
     ADD CONSTRAINT customer_owned_registrations_registration_fkey FOREIGN KEY (owned_registrations_registration) REFERENCES registration (registration);
 
 ALTER TABLE ONLY customer_wishlisted_listings
-    ADD CONSTRAINT customer_wishlisted_listings_customer_fkey FOREIGN KEY (customer_username) REFERENCES customer (username);
+    ADD CONSTRAINT customer_wishlisted_listings_customer_fkey FOREIGN KEY (customer_id) REFERENCES customer (id);
 
 ALTER TABLE ONLY customer_wishlisted_listings
     ADD CONSTRAINT customer_wishlisted_listings_listing_fkey FOREIGN KEY (wishlisted_listings_registration_registration) REFERENCES listing (registration_registration);
