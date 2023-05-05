1b64589750cf4fe2de339b0c890b2779eaceb655
CB222124 <2222124@student.uwtsd.ac.uk>
2023-05-05 20:03:18 +0100

    Delay API key resolution

--- backend/src/main/java/com/github/cb2222124/vlpms/backend/service/VesService.java
+++ backend/src/main/java/com/github/cb2222124/vlpms/backend/service/VesService.java
@@ -29,10 +29,8 @@ public class VesService {
 
     public VesService() {
         webClient = WebClient.builder()
-                .baseUrl(URI)
                 .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .defaultHeader("x-api-key", KEY)
                 .build();
     }
 
@@ -45,6 +43,8 @@ public class VesService {
     public VesResponse queryRegistration(String registration) {
         String request = new JSONObject().appendField("registrationNumber", registration).toJSONString();
         return webClient.post()
+                .uri(URI)
+                .header("x-api-key", KEY)
                 .header(HttpHeaders.CONTENT_LENGTH, String.valueOf(request.getBytes(StandardCharsets.UTF_8).length))
                 .bodyValue(request)
                 .retrieve()
